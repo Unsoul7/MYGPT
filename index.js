aac72561746407e9d9216f3e354e3c0316ba0d76
unsoul7 <disketteit@gmail.com>
2025-02-06 03:08:46 +0530

    Initialize project with Express server, EJS views, and basic styling; add .gitignore and package.json

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 const express = require('express');
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-
-const genAI = new GoogleGenerativeAI("AIzaSyDQwWUrMrLX5JvrzFJie4qTAXyI896NvXc");
-
 const app = express();
 const port = 3000;
 
@@ -10,7 +6,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(express.json());
 app.set('view engine', 'ejs');
-app.use('/api', require('./routes/genai'));
+app.use('./api', require('./routes/genai'));
 
 app.get('/', async (req, res) => {
     res.render('index');
